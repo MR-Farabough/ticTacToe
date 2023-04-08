f96e08c63bc1e04eb733f57605ea2a1e0f0ceec1
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-08 13:25:05 -0500

    Proper revealing module design

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const Game = {
-	getName: function () {
+const Game = (function () {
+	function getName() {
 		const name = 'Player'; // prompt('Player Name?');
 		const nameEL = document.querySelector('.player');
 		if (name === null || name.length < 2) {
@@ -7,16 +7,16 @@ const Game = {
 		} else {
 			nameEL.textContent = name;
 		}
-	},
-	getBot: function () {
+	}
+	function getBot() {
 		const botName = `BOT${Math.floor(Math.random() * 1000)}`;
 		const botEL = document.querySelector('.bot');
 		botEL.textContent = botName;
-	},
-	render: function () {
-		this.getName();
-		this.getBot();
-	},
-};
-
-Game.render();
+	}
+	function render() {
+		getName();
+		getBot();
+	}
+	render();
+	return { getName, getBot };
+})();
