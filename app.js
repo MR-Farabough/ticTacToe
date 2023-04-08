be275f134b044347bb5e930125c1b0b37e7eba9d
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-08 14:59:07 -0500

    Highlighted Names functionality

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const Game = (function () {
 	const nameEL = document.querySelector('.player');
 	const container = document.getElementById('contianer');
 	const card = document.querySelectorAll('.card');
+	const errMsg = document.querySelector('.errorMSG');
 
 	function getName() {
 		const name = 'Player'; // prompt('Player Name?');
-
 		if (name === null || name.length < 2) {
 			nameEL.textContent = 'Player 1';
 		} else {
@@ -24,11 +24,21 @@ const Game = (function () {
 			card.addEventListener('click', () => {
 				// Add Image
 				let imgs = document.createElement('img');
+				// Card Validation
 				if (card.children.length >= 1) {
+					errMsg.style.color = 'red';
+					errMsg.style.fontWeight = 'bold';
+					errMsg.style.fontSize = '20px';
+					errMsg.textContent = "YOU CAN'T GO THERE!";
 					return;
 				} else {
 					switch (turn) {
 						case 'playerTurn':
+							errMsg.textContent = '';
+							botEL.style.fontWeight = 'bold';
+							botEL.style.color = 'green';
+							nameEL.style.color = 'black';
+							nameEL.style.fontWeight = 'normal';
 							imgs.src = './imgs/redX.jpg';
 							imgs.style.width = '50px';
 							imgs.style.height = '50px';
@@ -37,6 +47,11 @@ const Game = (function () {
 							turn = 'botTurn';
 							break;
 						case 'botTurn':
+							errMsg.textContent = '';
+							botEL.style.color = 'black';
+							nameEL.style.color = 'green';
+							botEL.style.fontWeight = 'normal';
+							nameEL.style.fontWeight = 'bold';
 							imgs.src = './imgs/blackO.jpg';
 							imgs.style.width = '50px';
 							imgs.style.height = '50px';
