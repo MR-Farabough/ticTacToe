99abbd2833e7b1ac595f11a8670256955d9197e4
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-09 00:51:53 -0500

    Logic for win completed

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const Game = (function () {
 	const errMsg = document.querySelector('.errorMSG');
 	let cards = Array.from(cardNode);
 	let takenCards = [];
+	let gameOver = false;
+	let playerScore = 0;
+	let botScore = 0;
 
 	function getName() {
 		const name = 'Player'; // prompt('Player Name?');
@@ -22,10 +25,10 @@ const Game = (function () {
 	function createX(curCard) {
 		let imgs = document.createElement('img');
 		errMsg.textContent = '';
-		botEL.style.fontWeight = 'bold';
-		botEL.style.color = 'green';
-		nameEL.style.color = 'black';
-		nameEL.style.fontWeight = 'normal';
+		botEL.style.color = 'black';
+		botEL.style.fontWeight = 'normal';
+		nameEL.style.color = 'green';
+		nameEL.style.fontWeight = 'bold';
 		imgs.src = './imgs/redX.jpg';
 		imgs.style.width = '50px';
 		imgs.style.height = '50px';
@@ -35,10 +38,10 @@ const Game = (function () {
 	function createO(curCard) {
 		let imgs = document.createElement('img');
 		errMsg.textContent = '';
-		botEL.style.color = 'black';
-		botEL.style.fontWeight = 'normal';
-		nameEL.style.color = 'green';
-		nameEL.style.fontWeight = 'bold';
+		botEL.style.fontWeight = 'bold';
+		botEL.style.color = 'red';
+		nameEL.style.color = 'black';
+		nameEL.style.fontWeight = 'normal';
 		imgs.src = './imgs/blackO.jpg';
 		imgs.style.width = '50px';
 		imgs.style.height = '50px';
@@ -49,7 +52,7 @@ const Game = (function () {
 		let turn = 'botTurn';
 		cardNode.forEach((curCard) => {
 			curCard.addEventListener('click', () => {
-				if (curCard.children.length >= 1) {
+				if (curCard.children.length >= 1 && !gameOver) {
 					errMsg.style.color = 'red';
 					errMsg.style.fontWeight = 'bold';
 					errMsg.style.fontSize = '20px';
@@ -58,18 +61,27 @@ const Game = (function () {
 				} else {
 					switch (turn) {
 						case 'playerTurn':
-							createX(curCard);
-							updateSquares();
-							console.log(takenCards);
-							turn = 'botTurn';
-							checkGameOver();
+							if (gameOver == true) {
+								return;
+							} else {
+								createX(curCard);
+								updateSquares();
+								console.log(takenCards);
+								turn = 'botTurn';
+								console.log(cardNode);
+								checkGameOver();
+							}
 							break;
 						case 'botTurn':
-							createO(curCard);
-							updateSquares();
-							botMove();
-							turn = 'playerTurn';
-							checkGameOver();
+							if (gameOver == true) {
+								return;
+							} else {
+								createO(curCard);
+								updateSquares();
+								botMove();
+								turn = 'playerTurn';
+								checkGameOver();
+							}
 							break;
 					}
 				}
@@ -90,32 +102,115 @@ const Game = (function () {
 		// 0 | 1 | 2
 		// 3 | 4 | 5
 		// 6 | 7 | 8
-		// (258) | (147) | (036) | (012) | (345) | (678) | (048) | (246)
+		// (258) | (147) | (036)
+		const redX =
+			'<img src="./imgs/redX.jpg" class="redX" style="width: 50px; height: 50px;">';
+		const black0 =
+			'<img src="./imgs/blackO.jpg" class="blackO" style="width: 50px; height: 50px;">';
+		errMsg.style.color = 'black';
+		errMsg.style.fontWeight = 'bold';
+		errMsg.style.fontSize = '20px';
 		for (let index = 0; index < cardNode.length; index++) {
 			// Check vertical columns
 			if (
-				cardNode[index].innerHTML.length > 1 &&
-				cardNode[index + 3].innerHTML.length > 1 &&
-				cardNode[index + 6].innerHTML.length > 1
+				(cardNode[2].innerHTML == redX &&
+					cardNode[5].innerHTML == redX &&
+					cardNode[8].innerHTML == redX) ||
+				(cardNode[1].innerHTML == redX &&
+					cardNode[4].innerHTML == redX &&
+					cardNode[7].innerHTML == redX) ||
+				(cardNode[0].innerHTML == redX &&
+					cardNode[3].innerHTML == redX &&
+					cardNode[6].innerHTML == redX)
 			) {
-				console.log('column true');
-				// Check horizontal rows
+				errMsg.textContent = 'RED column true';
+				errMsg.style.color = 'green';
+				botEL.style.color = 'red';
+				nameEL.style.color = 'black';
+				gameOver = true;
 			} else if (
-				cardNode[index].innerHTML.length > 1 &&
-				cardNode[index + 1].innerHTML.length > 1 &&
-				cardNode[index + 2].innerHTML.length > 1
+				(cardNode[2].innerHTML == black0 &&
+					cardNode[5].innerHTML == black0 &&
+					cardNode[8].innerHTML == black0) ||
+				(cardNode[1].innerHTML == black0 &&
+					cardNode[4].innerHTML == black0 &&
+					cardNode[7].innerHTML == black0) ||
+				(cardNode[0].innerHTML == black0 &&
+					cardNode[3].innerHTML == black0 &&
+					cardNode[6].innerHTML == black0)
 			) {
-				console.log('row true');
+				errMsg.textContent = 'BLACK column true';
+				errMsg.style.color = 'green';
+				botEL.style.color = 'black';
+				botEL.style.fontWeight = 'normal';
+				nameEL.style.fontWeight = 'bold';
+				nameEL.style.color = 'green';
+				return (gameOver = true);
+			} else if (
+				(cardNode[0].innerHTML == redX &&
+					cardNode[1].innerHTML == redX &&
+					cardNode[2].innerHTML == redX) ||
+				(cardNode[3].innerHTML == redX &&
+					cardNode[4].innerHTML == redX &&
+					cardNode[5].innerHTML == redX) ||
+				(cardNode[6].innerHTML == redX &&
+					cardNode[7].innerHTML == redX &&
+					cardNode[8].innerHTML == redX)
+			) {
+				errMsg.textContent = 'RED row true';
+				errMsg.style.color = 'green';
+				botEL.style.color = 'red';
+				nameEL.style.color = 'black';
+				return (gameOver = true);
 				// Check diagnol
 			} else if (
-				cardNode[0].innerHTML.length > 1 &&
-				cardNode[4].innerHTML.length > 1 &&
-				cardNode[8].innerHTML.length > 1
+				(cardNode[0].innerHTML == black0 &&
+					cardNode[1].innerHTML == black0 &&
+					cardNode[2].innerHTML == black0) ||
+				(cardNode[3].innerHTML == black0 &&
+					cardNode[4].innerHTML == black0 &&
+					cardNode[5].innerHTML == black0) ||
+				(cardNode[6].innerHTML == black0 &&
+					cardNode[7].innerHTML == black0 &&
+					cardNode[8].innerHTML == black0)
 			) {
-				console.log('diagnol true');
-				// No Win
+				errMsg.textContent = 'BLACK row true';
+				errMsg.style.color = 'green';
+				botEL.style.color = 'black';
+				botEL.style.fontWeight = 'normal';
+				nameEL.style.fontWeight = 'bold';
+				nameEL.style.color = 'green';
+				return (gameOver = true);
+			} else if (
+				(cardNode[0].innerHTML == redX &&
+					cardNode[4].innerHTML == redX &&
+					cardNode[8].innerHTML == redX) ||
+				(cardNode[2].innerHTML == redX &&
+					cardNode[4].innerHTML == redX &&
+					cardNode[6].innerHTML == redX)
+			) {
+				errMsg.textContent = 'RED diagnol true';
+				errMsg.style.color = 'green';
+				botEL.style.color = 'red';
+				nameEL.style.color = 'black';
+				return (gameOver = true);
+			} else if (
+				(cardNode[0].innerHTML == black0 &&
+					cardNode[4].innerHTML == black0 &&
+					cardNode[8].innerHTML == black0) ||
+				(cardNode[2].innerHTML == black0 &&
+					cardNode[4].innerHTML == black0 &&
+					cardNode[6].innerHTML == black0)
+			) {
+				errMsg.textContent = 'BLACK diagnol true';
+				errMsg.style.color = 'green';
+				botEL.style.color = 'black';
+				botEL.style.fontWeight = 'normal';
+				nameEL.style.fontWeight = 'bold';
+				nameEL.style.color = 'green';
+				return (gameOver = true);
 			} else {
-				console.log('no win yet');
+				errMsg.textContent = 'no win yet';
 			}
 		}
 	}
