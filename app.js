1346b9c1004c463033c03ff935143ce49f6db5d7
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-08 21:25:22 -0500

    takenCards array contains taken cards

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const Game = (function () {
 	// Cahche the dom
 	const botEL = document.querySelector('.bot');
 	const nameEL = document.querySelector('.player');
-	const container = document.getElementById('contianer');
-	const card = document.querySelectorAll('.card');
+	const cardNode = document.querySelectorAll('.card');
 	const errMsg = document.querySelector('.errorMSG');
+	let cards = Array.from(cardNode);
+	let takenCards = [];
 
 	function getName() {
 		const name = 'Player'; // prompt('Player Name?');
@@ -20,12 +21,12 @@ const Game = (function () {
 	}
 	function cardListener() {
 		let turn = 'playerTurn';
-		card.forEach((card) => {
-			card.addEventListener('click', () => {
+		cardNode.forEach((curCard) => {
+			curCard.addEventListener('click', () => {
 				// Add Image
 				let imgs = document.createElement('img');
-				// Card Validation
-				if (card.children.length >= 1) {
+				// curCard Validation
+				if (curCard.children.length >= 1) {
 					errMsg.style.color = 'red';
 					errMsg.style.fontWeight = 'bold';
 					errMsg.style.fontSize = '20px';
@@ -43,26 +44,39 @@ const Game = (function () {
 							imgs.style.width = '50px';
 							imgs.style.height = '50px';
 							imgs.classList.add('redX');
-							card.append(imgs);
+							curCard.append(imgs);
 							turn = 'botTurn';
+							updateSquares();
+							console.log(takenCards);
 							break;
 						case 'botTurn':
 							errMsg.textContent = '';
 							botEL.style.color = 'black';
-							nameEL.style.color = 'green';
 							botEL.style.fontWeight = 'normal';
+							nameEL.style.color = 'green';
 							nameEL.style.fontWeight = 'bold';
 							imgs.src = './imgs/blackO.jpg';
 							imgs.style.width = '50px';
 							imgs.style.height = '50px';
 							imgs.classList.add('blackO');
-							card.append(imgs);
+							curCard.append(imgs);
 							turn = 'playerTurn';
+							updateSquares();
+							console.log(takenCards);
 							break;
 					}
 				}
 			});
 		});
+	}
+	function updateSquares() {
+		let numArr = [];
+		cardNode.forEach((card) => {
+			if (card.innerHTML.length > 5) {
+				numArr.push(cards.indexOf(card));
+			}
+		});
+		takenCards = numArr;
 	}
 	function render() {
 		getName();
@@ -70,5 +84,6 @@ const Game = (function () {
 		cardListener();
 	}
 	render();
+
 	return { getName, getBot };
 })();
